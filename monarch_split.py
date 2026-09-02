cf970df44576414abefeba62bc75188acb610e2a
jakelig <43222956+jakelig@users.noreply.github.com>
2026-09-01 22:53:53 -0400

    Update monarch_split.py

--- monarch_split.py
+++ monarch_split.py
@@ -57,8 +57,6 @@ async def split():
         pl.col('merchant').alias('merchantName')
     )
 
-    print(transactions)
-
     found_cats = set(transactions['category'])
     configured_cats = set([split['category'] for split in splits['components']])
     
